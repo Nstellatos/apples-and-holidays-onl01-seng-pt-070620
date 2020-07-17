18bb7fff6360e444bffae607f1479a793d5d4f81
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-17 23:00:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,7 +86,11 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-  
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, item|
+      holiday if item.include?("BBQ")
+    end
+  end
   
   
   
