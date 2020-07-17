d726b96baeb519fd55cc2ef6fa3a529e9e4bc5ac
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-17 22:11:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_supplies[:fall] << [holiday_name:"Flags", "Parade Floats", "Italian Food"]
+  holiday_supplies[:fall] << holiday_name
   
   
   # code here
