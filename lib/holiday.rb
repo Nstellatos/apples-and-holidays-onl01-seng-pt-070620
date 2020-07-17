7c31dea449a04001dd52301745d5091c4a87d9e7
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-17 22:28:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,8 +55,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter] >> "Lights"
-  holiday_hash[:winter].values
+  holiday_hash[:wnter].values
   end
   
 def all_supplies_in_holidays(holiday_hash)
