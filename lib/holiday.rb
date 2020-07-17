c05af85c0bdf7606c00176a914393815dd044b9a
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-17 22:58:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,6 +73,7 @@ def all_supplies_in_holidays(holiday_hash)
       puts "  #{holiday}: #{supply}"
 end 
 end
+end
 
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
