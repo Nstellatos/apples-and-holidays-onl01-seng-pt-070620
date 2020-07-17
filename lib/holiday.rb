5712acfd2a9f8c013f9d8fdea2d1d8cc7796a011
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-17 22:47:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holiday, item|
        puts "  #{holiday.to_s.split("_").map 
        {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
-  end
+  
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
